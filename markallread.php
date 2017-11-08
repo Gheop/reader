4d922c966aa10f5b5bcc0bf53afb46c337f13b55
SiB <lizardonthestorm@gmail.com>
2017-11-08 16:02:47 +0100

    Correction de la récupération des $_POST

--- markallread.php
+++ markallread.php
@@ -1,14 +1,9 @@
 <?php
 include('/www/conf.php');
-$r = $mysqli->query('select RI.id from reader_item RI where RI.id_flux='.$_POST['f'].' and RI.id not in (select id_item from reader_user_item as UI where UI.id_user='.$_SESSION['user_id'].')') or die($mysqli->error);
-//$t = array();
+if(!isset($_POST['f']) || !is_numeric($_POST['f']) || !isset($_SESSION['user_id']) || !is_numeric($_SESSION['user_id'])) exit;$r = $mysqli->query('select RI.id from reader_item RI where RI.id_flux='.$_POST['f'].' and RI.id not in (select id_item from reader_user_item as UI where UI.id_user='.$_SESSION['user_id'].')') or die($mysqli->error);
 $query = '';
 while($e = $r->fetch_array()) {
- // $t[] = $e[0];
   $query .= "insert into reader_user_item (id_user, id_item, date) values($_SESSION[user_id], $e[0], now());";
 }
 $mysqli->multi_query($query);
-// foreach($t as $i) {
-//   $mysqli->query("insert into reader_user_item (id_user, id_item, date) values($_SESSION[user_id], $i, now());") or die($mysqli->error);
-// }
 ?>
