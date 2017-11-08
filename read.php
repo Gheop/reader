c209530c2e242a9a80eb8774b08f1e41dda86b5a
SiB <lizardonthestorm@gmail.com>
2017-11-08 16:20:23 +0100

    Correction des $_POST

--- read.php
+++ read.php
@@ -1,5 +1,5 @@
 <?php
 include('/www/conf.php');
 if(!isset($_POST['id']) || !is_numeric($_POST['id']) || !isset($_SESSION['user_id']) || !is_numeric($_SESSION['user_id'])) exit;
-$mysqli->query("INSERT IGNORE INTO reader_user_item (id_user, id_item, date) VALUES ($_SESSION['user_id'], $_POST['id'], now());");
+$mysqli->query("INSERT IGNORE INTO reader_user_item (id_user, id_item, date) VALUES ($_SESSION[user_id], $_POST[id], now());");
 ?>
