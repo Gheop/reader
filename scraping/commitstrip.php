f58f2131843e76757e2b9aa7e0623c4e8716efc5
SiB <lizardonthestorm@gmail.com>
2018-10-19 20:45:23 +0200

    Correction du scraping pour commitstrip.fr

--- scraping/commitstrip.php
+++ scraping/commitstrip.php
@@ -51,6 +51,7 @@ foreach($html->find('div[class=excerpt] section a') as $element) {
 	}
 	foreach($detail->find('div[class=entry-content] p img') as $info) {
 		$mydescription = '<img src="'.$info->src.'" />';
+		break;
 	}
 
 
@@ -61,59 +62,9 @@ foreach($html->find('div[class=excerpt] section a') as $element) {
 	      <guid>".$element->href."</guid>
     </item>
 ";
-$detail->clear(); 
+$detail->clear();
 unset($detail);
 }
-/*   exit;
-
-
-$p = file_get_contents($url);
-//echo $p;
-preg_match_all('/<a title="Télécharger .*?" href="(.*)?" /im', $p, $m);
-
-$i=0;
-$mh = curl_multi_init();
-$ch = array();
-//$dd = array();
-$i = 0;
-
-while(isset($m[1][$i])) {
-	$ch[$i] = curl_init();
-	curl_setopt_array($ch[$i],
-		Array(
-			CURLOPT_URL => $base_url.$m[1][$i],
-			CURLOPT_USERAGENT => 'GheopReader',
-			CURLOPT_TIMEOUT => 5,
-			CURLOPT_CONNECTTIMEOUT => 10,
-			CURLOPT_RETURNTRANSFER => TRUE,
-			CURLOPT_ENCODING => 'UTF-8'
-			)
-		);
-	curl_multi_add_handle($mh, $ch[$i]);
-	//$dd[$i] = $m[2][$i];
-	$i++;
-}
-
-$running=null;
-
-do {
-	curl_multi_exec($mh,$running);
-	//usleep (1000);
-} while ($running > 0);
-
-for($j=0;$j<$i;$j++) {
-	//if($_POST['f'] == 'films')
-		preg_match('/<h5 class="pull-left" style="max-width:inherit"><i class="fa fa-.*"><\/i> (.*)<\/h5>.*<\/strong><\/p>.*<p>(.*)<\/p>.*<a class="btn btn-danger download" href="(.*)">/smiU', curl_multi_getcontent($ch[$j]), $z);
-
-		if(isset($z[1]))
-	echo "    <item>
-      <title>",htmlspecialchars(stripslashes($z[1]),ENT_QUOTES,'UTF-8'),"</title>
-      <description>",(isset($z[2])?htmlspecialchars(stripslashes($z[2]),ENT_QUOTES,'UTF-8'):""),"</description>
-      <link>$base_url".(isset($z[3])?$z[3]:"")."</link>
-      <guid>$base_url".(isset($z[3])?$z[3]:"")."</guid>
-    </item>
-";
-}*/
 ?>
   </channel>
 </rss>
