1a804ca845ab9d815e3aa13469ca68959b32c957
SiB <lizardonthestorm@gmail.com>
2020-04-10 09:36:22 +0200

    remove old files

--- dl_torrent.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_REQUEST['f'])) exit;
-
-$file = $_REQUEST['f'];
-if($data = file_get_contents($file)) {
-    $tmpfile='/ftp/Torrents/'.md5($data).'.torrent';
-    file_put_contents($tmpfile, $data);
-}
