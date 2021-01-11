76c43b29d6d947027aec2823144e6edaaa02369e
SiB <lizardonthestorm@gmail.com>
2021-01-11 10:22:48 +0100

    Version number support! Just print in dev version.

--- index.php
+++ index.php
@@ -63,6 +63,7 @@ else {
     $firstLine = $stringfromfile[0];
     $explodedstring = explode("/", $firstLine, 3);
     $branchname = trim($explodedstring[2]);
+    if(file_exists('version')) $branchname .= ' '.file_get_contents('version');
     if($branchname != 'master')    echo "<span style='font-family: Helvetica; color: #d43f57; position: relative;bottom: 1px;font-size: .4em;line-height: .4em;vertical-align:super;text-decoration:none;'>" . $branchname . "</span>";
   }
   ?>
